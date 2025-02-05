63ef9a2497ffab8017932a4f07e92918da8d774b
Alex Bunting <alex.bunting314@gmail.com>
2025-02-06 09:41:36 +1100

    Disconnect google sign in on logout so next sign in shows account selection screen

--- lib/repositories/user_repo.dart
+++ lib/repositories/user_repo.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -27,6 +28,24 @@ class UserRepo extends _$UserRepo {
 
   void logout() {
     ref.read(firebaseAuthProvider).signOut();
+
+    // In the Flutter Firebase Authentication package for mobile clients, the first time a user
+    // signs in with google they are always shown an account selection screen (even if they only
+    // have one account). Once the user has logged in with google once, if they then sign out and
+    // then signs in again, they are no longer shown the account selection screen. Instead they will
+    // be automatically logged in with the same account as before.
+    //
+    // It is preferable to always show the account selection screen. During testing, it makes it
+    // easier to switch between different test accounts. In production, it allows users to maintain
+    // different accounts for different purposes. The only drawback is that if a user has signed in
+    // once on a device and then signs out accidentally, they will have to select their account
+    // again when they sign in, which overall seems an insignificant impact compared to the
+    // increased flexibility and ease of testing.
+    //
+    // To force showing the account selection again, we disconnect the google sign in account. See
+    // https://github.com/flutter/flutter/issues/161476#issuecomment-2591358974
+    // This method has no effect if the user is not signed in with Google.
+    GoogleSignIn().disconnect();
   }
 
   void setUserName(String name) {
