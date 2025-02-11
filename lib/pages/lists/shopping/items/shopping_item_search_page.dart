d6dc45e4c978780cdce7024269200cc4b4961a72
Alex Bunting <alex.bunting314@gmail.com>
2025-02-11 17:58:44 +1100

    Add tab view to new item page with second optional tab for selecting item category

--- lib/pages/lists/shopping/items/shopping_item_search_page.dart
+++ lib/pages/lists/shopping/items/shopping_item_search_page.dart
@@ -4,43 +4,122 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'item_search_view_model.dart';
 import 'shopping_item_view.dart';
 
-class ShoppingItemSearchPage extends ConsumerWidget {
+class ShoppingItemSearchPage extends ConsumerStatefulWidget {
   const ShoppingItemSearchPage({required this.listId, super.key});
   final String listId;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final filterValue = ref.watch(itemFilterProvider);
+  ConsumerState<ShoppingItemSearchPage> createState() => _ShoppingItemSearchPageState();
+}
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('New Item'),
-        actions: const [
-          ShoppingItemTooltipAction(),
-        ],
+class _ShoppingItemSearchPageState extends ConsumerState<ShoppingItemSearchPage>
+    with SingleTickerProviderStateMixin {
+  late final tabController = TabController(length: 2, vsync: this);
+  String? nameError;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: tabController.index == 0,
+      onPopInvokedWithResult: (didPop, _) {
+        if (didPop) {
+          return;
+        }
+        tabController.animateTo(0);
+        setState(() {
+          // This triggers a rebuild so the PopScope can update its canPop value
+        });
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('New Item'),
+          actions: const [
+            ShoppingItemTooltipAction(),
+          ],
+        ),
+        body: TabBarView(
+          controller: tabController,
+          physics: const NeverScrollableScrollPhysics(),
+          children: [
+            ShoppingItemSearchView(onCategoryRequired: () {
+              tabController.animateTo(1);
+              setState(() {
+                // This triggers a rebuild so the PopScope can update its canPop value
+              });
+            }),
+            const ShoppingItemCategorySelectView(),
+          ],
+        ),
       ),
-      body: Column(children: [
-        Padding(
-          padding: const EdgeInsets.all(16),
-          child: TextField(
-            decoration: const InputDecoration(
-              labelText: 'Enter item name',
-            ),
-            onChanged: (newValue) {
-              ref.read(itemFilterProvider.notifier).setFilter(newValue);
-            },
-          ),
-        ),
-        Expanded(
-          child: filterValue.isEmpty ? const SuggestionsPlaceholderView() : const SuggestionsView(),
-        ),
-      ]),
     );
   }
 }
 
-class SuggestionsView extends ConsumerWidget {
-  const SuggestionsView({super.key});
+class ShoppingItemSearchView extends ConsumerStatefulWidget {
+  const ShoppingItemSearchView({required this.onCategoryRequired, super.key});
+  final VoidCallback onCategoryRequired;
+
+  @override
+  ConsumerState<ShoppingItemSearchView> createState() => _ShoppingItemSearchViewState();
+}
+
+class _ShoppingItemSearchViewState extends ConsumerState<ShoppingItemSearchView> {
+  final _nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final filterValue = ref.watch(itemFilterProvider);
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
+        child: TextField(
+          decoration: const InputDecoration(
+            labelText: 'Enter item name',
+          ),
+          autofocus: true,
+          controller: _nameController,
+          onChanged: (newValue) {
+            ref.read(itemFilterProvider.notifier).setFilter(newValue);
+          },
+        ),
+      ),
+      Expanded(
+        child:
+            filterValue.isEmpty ? const ItemSuggestionsPlaceholder() : const ItemSuggestionsList(),
+      ),
+      Material(
+        elevation: 4,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextButton.icon(
+              onPressed: _onAddMore,
+              icon: const Icon(Icons.add),
+              label: const Text('Add more'),
+            ),
+            TextButton.icon(
+              onPressed: _onDone,
+              icon: const Icon(Icons.check),
+              label: const Text('Done'),
+            ),
+          ],
+        ),
+      ),
+    ]);
+  }
+
+  void _onDone() {
+    print('On done');
+    widget.onCategoryRequired();
+  }
+
+  void _onAddMore() {
+    print('On add more');
+  }
+}
+
+class ItemSuggestionsList extends ConsumerWidget {
+  const ItemSuggestionsList({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -53,7 +132,7 @@ class SuggestionsView extends ConsumerWidget {
     }
     final suggestions = suggestionsAsync.requireValue;
     if (suggestions.isEmpty) {
-      return const SuggestionsEmptyView();
+      return const ItemSuggestionsEmpty();
     }
     return ListView.builder(
       itemCount: suggestions.length,
@@ -69,8 +148,8 @@ class SuggestionsView extends ConsumerWidget {
   }
 }
 
-class SuggestionsPlaceholderView extends StatelessWidget {
-  const SuggestionsPlaceholderView({super.key});
+class ItemSuggestionsPlaceholder extends StatelessWidget {
+  const ItemSuggestionsPlaceholder({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -78,7 +157,7 @@ class SuggestionsPlaceholderView extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       children: [
         ListTile(
-          leading: Icon(Icons.recommend_outlined),
+          leading: Icon(Icons.search),
           title: Text('Start typing to search suggested items and your item history'),
         ),
         ListTile(
@@ -96,13 +175,51 @@ class SuggestionsPlaceholderView extends StatelessWidget {
   }
 }
 
-class SuggestionsEmptyView extends StatelessWidget {
-  const SuggestionsEmptyView({super.key});
+class ItemSuggestionsEmpty extends StatelessWidget {
+  const ItemSuggestionsEmpty({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Center(
       child: Text('No suggestions found'),
     );
+  }
+}
+
+class ShoppingItemCategorySelectView extends ConsumerStatefulWidget {
+  const ShoppingItemCategorySelectView({super.key});
+
+  @override
+  ConsumerState<ShoppingItemCategorySelectView> createState() =>
+      _ShoppingItemCategorySelectViewState();
+}
+
+class _ShoppingItemCategorySelectViewState extends ConsumerState<ShoppingItemCategorySelectView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Expanded(
+          child: Center(child: Text('Category selector')),
+        ),
+        Material(
+          elevation: 4,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton.icon(
+                onPressed: _onDone,
+                icon: const Icon(Icons.check),
+                label: const Text('Done'),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _onDone() {
+    print('On done');
   }
 }
