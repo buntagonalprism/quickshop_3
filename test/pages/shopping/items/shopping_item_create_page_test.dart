b35f0da4f2b5e1ba7572a3353af5e994a2e1edc0
Alex Buntowe <alex.bunting314@gmail.com>
2025-10-21 22:11:59 +1100

    Support synchronous methods in Answerer helper class

--- test/pages/shopping/items/shopping_item_create_page_test.dart
+++ test/pages/shopping/items/shopping_item_create_page_test.dart
@@ -399,18 +399,37 @@ Answerer<T> answer<T>(Future<T> Function() fn) {
   return Answerer<T>(fn);
 }
 
+/// Helper class that saves a few characters when answering mocked methods
+/// and avoids the need to remember whether to use [thenAnswer] or [thenReturn].
 class Answerer<T> {
-  final Future<T> Function() _fn;
+  final FutureOr<T> Function() _fn;
   Answerer(this._fn);
+
   void withValue(T value) {
-    when(_fn).thenAnswer((_) => Future.value(value));
+    if (_fn is Future<T> Function()) {
+      when(_fn).thenAnswer((_) => Future.value(value));
+    } else {
+      when(_fn).thenReturn(value);
+    }
   }
 
   void withLoading() {
-    when(_fn).thenAnswer((_) => Completer<T>().future);
+    if (_fn is Future<T> Function()) {
+      when(_fn).thenAnswer((_) => Completer<T>().future);
+    } else {
+      throw UnsupportedError('Loading state is only supported for methods that return a future');
+    }
   }
 
   void withError(Object error) {
-    when(_fn).thenAnswer((_) => Future<T>.error(error));
+    if (_fn is Future<T> Function()) {
+      when(_fn).thenAnswer((_) => Future<T>.error(error));
+    } else {
+      when(_fn).thenThrow(error);
+    }
+  }
+
+  void byInvocation(Future<T> Function(Invocation) fn) {
+    when(_fn).thenAnswer(fn);
   }
 }
